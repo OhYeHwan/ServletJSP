876f913fb0571710b72f77de341d8e3cd0f8ace1
OOHYEE <azss0470@gmail.com>
2021-08-23 18:34:13 +0900

    [WebServlet] 어노테이션 추가

--- src/com/yh/web/Nana.java
+++ src/com/yh/web/Nana.java
@@ -8,12 +8,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+@WebServlet("/hi")
 public class Nana extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("Hello ~~!!");
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { // request : 입력 , response : 출력
+        response.setCharacterEncoding("UTF-8"); // 사용자에게 보내는 정보
+        response.setContentType("text/html; charset=UTF-8"); // 웹브라우저에게 이 정보로 읽으라고 알려줌
+
+        PrintWriter out = response.getWriter();
+        String tmp = request.getParameter("cnt");
+        int cnt = 100;
+        if(tmp != null && !tmp.equals("")) {
+            cnt = Integer.parseInt(tmp);
+        }
+
+        for(int i=0; i<cnt; i++) {
+            out.println((i+1)+ ": 안녕 Servlet!!<br >");
+        }
     }
 }
 
